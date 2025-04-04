1af71e481f69e3bfb51cfc200e03d77c4f47b07a
nikitalokhmachev-ai <n1kkqt@gmail.com>
2025-04-04 16:31:05 -0400

    feat: allow host

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		allowedHosts: ["terminology-resort-basename-contents.trycloudflare.com"],
+		allowedHosts: ["nikitalokhmachev-ai.github.io"],
 	},
 	base: "/side-stacker-frontend",
 });
